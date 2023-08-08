2b0f3cf5b960157cb8c4a4837930408b5435ebad
2F4U-LB <58055050+2F4U-LB@users.noreply.github.com>
2023-08-07 20:50:55 -0700

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,22 +1,7 @@
 import main
-if main.act==1:
-        verify=input("\n")
-        if(verify==main.pw):
-                print(f"\nHello, {main.king}.\n")
-                main.init()
-                while(True):
-                        cmd=input()
-                        if(cmd[:4].lower()=="exit"):
-                                print(f"\nGoodbye, {main.king}.\n")
-                                exit()
-
-                        elif(cmd[:4].lower()=="time"):
-                                import time
-                                print("\n",time.ctime(time.time()),"\n")
-                        #elif(cmd[:3].lower()=="rsh"):
-                  
-                        else:
-                                print("\nI dont understand.\n")
-
-        else:
-                print(f"Invalid!\n")
+if main.act==0:
+        usr=input("\nUsername: ")
+        pw=input("\nPassword: ")
+        main.init(usr, pw)
+elif main.act=1:
+        print("")
