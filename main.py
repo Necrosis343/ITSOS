9f29cbee17c70a57bc1fde412a5fe00bdd51c093
2F4U-LB <58055050+2F4U-LB@users.noreply.github.com>
2023-08-07 20:06:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import users
+import pw
 def init():
     print("""
  ___   _______  _______
@@ -10,4 +11,13 @@ def init():
 |___| |_______|  |___| Founded, by Mikhael.
 """)
 def login():
+    inp=input("\nUsername: ")
+    if inp==users.king:
+        inp=input("\nPassword: ")
+    elif inp ==users.subject:
+        inp=input("\nPassword: ")
+        if inp=
+    else:
+        inp=input("\nPassword: ")
+        print("Invalid!")
 act=1
