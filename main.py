92f54c3253a9fae3f98e4e52c56a2d6765e71b02
2F4U-LB <58055050+2F4U-LB@users.noreply.github.com>
2023-08-09 09:18:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import users
-import pw
 def init(usr, pw):
     print("""
  ___   _______  _______  _______  _______ 
@@ -10,17 +8,3 @@ def init(usr, pw):
 |   |  _____| |  |   |  |       | _____| |
 |___| |_______|  |___|  |_______||_______| Founded, by Mikhael.
 """)
-    if usr==king.name:
-        if pw==king.pw:
-            print(f"Hello, {king.name}.")
-            act=1
-        else:
-            print("Invalid!")
-    elif usr==sub.name:
-        if pw==sub.pw:
-            print("Hello, {sub.name}.")
-            act=2
-        else:
-            print("Invalid!")
-    else:
-        print("Invalid!")
